513a978e4aa6d9f0992d4dd46b7494da4a4499e4
zarigani-design-office <nishihara.tsubasa77@gmail.com>
2019-04-24 18:01:10 +0900

    README修正加筆

--- README.md
+++ README.md
@@ -3,31 +3,44 @@
 このスクリプトはページ遷移をAjaxのような雰囲気にします。あくまで**Ajax風**であって、実際にAjaxを実装できるわけではないのでご注意ください。ページ間遷移をそれっぽくエフェクトをかけるスクリプトです。
 
 ## 使い方
-`dist`フォルダにある`pseudo_ajax.min.js`を読み込み、scriptタグ内で`pseudo_ajax()`を実行すればOK。読み込みがふわっと出現するようになり、aタグをクリックするとふわっと消えてページ遷移します。
+`dist`フォルダにある`pseudo_ajax.min.js`を`</body>`タグ直前に読み込みます。（注意：`<head>`タグ内だとエラーになります。）続く`<script>`タグ内で`pseudo_ajax()`を実行すればOKです。読み込み時にふわっと出現するようになり、aタグをクリックするとふわっと消えてページ遷移します。
 
 ```
 <script src="PATH/TO/pseudo-ajax.js"></script>
 <script>
   pseudo_ajax()
 </script>
+</body>
 ```
 
 ## API
 以下のAPIを用意しています。
 
 | API | Type | Default | 説明 |
-|----|----|----|----|
+|---|---|---|---|
 | tag | String | "a" | Ajax風を実行するセレクター |
+| loadingText | String | "Loading" | ローディング中の表示文字 |
 | loadingStyle | String | "position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);text-align:center" | Loadingの文字のスタイル |
+| beforeLoadStyle | String | "transform:translateY(16px);opacity:0" | 読み込み完了前のbodyに対するスタイル |
+| afterLoadStyle | String | "transition:all 0.6s ease-in-out;transform:translateY(0px);opacity:1" | 読み込み完了時のbodyに対するスタイル |
+| transitionStyle | String | "transition:all 0.6s ease-in-out;transform:translateY(16px);opacity:0" | ページ遷移時のbodyに対するスタイル |
 
-
+### 設定例
 
 ```
   pseudo_ajax({
     tag: ".pseudo",
+    loadingText: "LOADING",
     loadingStyle: "position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);text-align:center;font-size:2rem",
     beforeLoadStyle: "transform:translateY(48px);opacity:0",
     afterLoadStyle: "transition:all 1.5s ease-in-out;transform:translateY(0px);opacity:1",
     transitionStyle: "transition:all 2.6s ease-in-out;transform:translateY(32px);opacity:0"
   })
 ```
+
+## 対応ブラウザ
+
+ES6で書いているので、IE11以外のモダンなブラウザなら対応していはずです。
+
+## ライセンス
+MIT License
--- src/pseudo-ajax.js
+++ src/pseudo-ajax.js
@@ -3,25 +3,52 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-//読み込み時のアニメーション
-function pseudo_ajax(
-  settings = {
-    tag: "a",
-    loadingStyle:
-      "position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);text-align:center",
-    beforeLoadBodyStyle: "transform:translateY(16px);opacity:0",
-    afterLoadBodyStyle:
-      "transition:all 0.6s ease-in-out;transform:translateY(0px);opacity:1"
+function pseudoAjax(
+  //ユーザー設定読み込み デフォルト値はundefinedとして定義
+  yourSettings = {
+    tag: undefined,
+    loadingText: undefined,
+    loadingStyle: undefined,
+    beforeLoadStyle: undefined,
+    afterLoadStyle: undefined,
+    transitionStyle: undefined
   }
+) {
+  const yourTag = yourSettings.tag;
+  const yourLoadingText = yourSettings.loadingText;
+  const yourLoadingStyle = yourSettings.loadingStyle;
+  const yourBeforeLoadStyle = yourSettings.beforeLoadStyle;
+  const yourAfterLoadStyle = yourSettings.afterLoadStyle;
+  const yourTransitionStyle = yourSettings.transitionStyle;
+  //ユーザー設定を実行関数に渡す。
+  pseudoAjaxExe(
+    yourTag,
+    yourLoadingText,
+    yourLoadingStyle,
+    yourBeforeLoadStyle,
+    yourAfterLoadStyle,
+    yourTransitionStyle
+  );
+}
+//実行関数
+function pseudoAjaxExe(
+  //引数がundefinedの場合はデフォルト値
+  tag = "a",
+  loadingText = "Loading",
+  loadingStyle = "position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);text-align:center;font-family:sans-serif",
+  beforeLoadStyle = "transform:translateY(16px);opacity:0",
+  afterLoadStyle = "transition:all 0.6s ease-in-out;transform:translateY(0px);opacity:1",
+  transitionStyle = "transition:all 0.6s ease-in-out;transform:translateY(16px);opacity:0"
 ) {
   const body = document.getElementsByTagName("body")[0];
   const loadingDom = document.createElement("div");
+  //読み込み時のアニメーション
   document.addEventListener("DOMContentLoaded", () => {
     //DOM構築時Bodyフェードアウト
-    body.setAttribute("style", settings.beforeLoadBodyStyle);
+    body.setAttribute("style", beforeLoadStyle);
     //Loading文字
-    loadingDom.textContent = "Loading";
-    loadingDom.setAttribute("style", settings.loadingStyle);
+    loadingDom.textContent = loadingText;
+    loadingDom.setAttribute("style", loadingStyle);
     loadingDom.setAttribute("id", "loading");
     body.parentNode.insertBefore(loadingDom, body);
   });
@@ -32,32 +59,26 @@ function pseudo_ajax(
     //DOM構築完了時にフェードイン
     document
       .getElementsByTagName("body")[0]
-      .setAttribute("style", settings.afterLoadBodyStyle);
+      .setAttribute("style", afterLoadStyle);
   };
 
-  const selector = document.querySelectorAll(settings.tag);
-  //aタグクリック時にフェード付与
-  const aTags = Array.from(selector, e => {
-    return e;
-  });
-  aTags.forEach(a => {
-    const href = a.href;
-    const target = a.target;
-    a.addEventListener("click", e => {
+  //セレクタークリック時にフェード付与
+  const selectors = document.querySelectorAll(tag);
+  for (const selector of selectors) {
+    const href = selector.href;
+    const target = selector.target;
+    selector.addEventListener("click", e => {
+      //target="_blank"の場合は通常挙動
       if (target !== "_blank") {
-        //target="_blank"の場合は通常挙動
         //デフォルト挙動キャンセル
         e.preventDefault();
         //const body = document.getElementsByTagName("body")[0];
-        body.setAttribute(
-          "style",
-          "transition:all 0.6s ease-in-out;transform:translateY(16px);opacity:0"
-        );
+        body.setAttribute("style", transitionStyle);
         //トランジッション終わったら、ページ遷移
         body.addEventListener("transitionend", () => {
           location.href = href;
         });
       }
     });
-  });
+  }
 }
